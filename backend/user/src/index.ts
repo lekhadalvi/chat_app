ec74eb386ebcf2721120dd110975df18b7f317da
Lekha <lekhadalvi08@gmail.com>
2026-06-23 15:52:52 +0530

    Add Redis configuration and connection logic

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
-import { createClient } from 'redis';
+import UserRoutes from './routes/User.route.js'
+import { connectRedis } from "./config/redis.js";
 dotenv.config()
 
 
@@ -9,10 +10,8 @@ const app = express();
 const PORT = process.env.PORT
 
 connectDB();
-export const redisClient = createClient({
-    url:process.env.REDIS_URL!
-})
-redisClient.connect().then(()=>console.log("connected to redis")).catch(console.error)
+connectRedis();
+app.use("api/v1",UserRoutes)
 
 app.listen(PORT ,()=>{
 console.log(`🚀 Server running on http://localhost:${PORT}`);
