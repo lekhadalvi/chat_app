004451afa9e57c0a83e190bc5e41440f2af57476
Lekha <lekhadalvi08@gmail.com>
2026-06-23 17:27:31 +0530

    Add RabbitMQ connection setup and integration in the backend

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import UserRoutes from './routes/User.route.js'
 import { connectRedis } from "./config/redis.js";
+import { connectRabbitmq } from './config/rabbitmq.js';
+
 dotenv.config()
 
 
@@ -11,6 +13,8 @@ const PORT = process.env.PORT
 
 connectDB();
 connectRedis();
+connectRabbitmq();
+
 app.use("api/v1",UserRoutes)
 
 app.listen(PORT ,()=>{
