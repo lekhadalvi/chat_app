b19958d477bc3f65fb9f274ac21e6199e439aabd
Lekha <lekhadalvi08@gmail.com>
2026-06-29 12:55:59 +0530

    Implement JWT token generation and enhance user verification flow in LoginController

--- backend/user/src/controllers/User.controller.ts
+++ backend/user/src/controllers/User.controller.ts
@@ -1,29 +1,65 @@
 import { TryCatch } from "../components/reuse_code.js";
 import { redisClient } from "../config/redis.js";
-import {publishToQueue} from "../config/rabbitmq.js"
+import { publishToQueue } from "../config/rabbitmq.js"
+import { User } from "../models/User.models.js";
+import { generateToken } from "../config/generateToken.js";
 
-export const LoginController = TryCatch(async(req,res)=>{
-    const {email} = req.body
+export const LoginController = TryCatch(async (req, res) => {
+    const { email } = req.body
 
     const rateLimitKey = `otp:rate:limit:${email}`
     const rateLimit = await redisClient.get(rateLimitKey);
 
-    if(rateLimit) {
-        res.status(409).json({message:"to many request at a time,please wait for some time"})
-        return ;
+    if (rateLimit) {
+        res.status(409).json({ message: "to many request at a time,please wait for some time" })
+        return;
     }
 
-    const otp = Math.floor(10000 + Math.random()* 9000).toString()
+    const otp = Math.floor(10000 + Math.random() * 9000).toString()
     const otpkey = `otp:${email}`
-    await redisClient.set(otpkey,otp,{EX:300})
-    await redisClient.set(rateLimitKey,"true",{EX:60})
+    await redisClient.set(otpkey, otp, { EX: 300 })
+    await redisClient.set(rateLimitKey, "true", { EX: 60 })
 
     const message = {
         email,
         otp,
     };
 
-    await publishToQueue("send-otp" ,message)
-    res.status(200).json({message:"otp send to your mail"})
+    await publishToQueue("send-otp", message)
+    res.status(200).json({ message: "otp send to your mail" })
 
 })
+
+export const VerifyController = TryCatch(async (req, res) => {
+    const { email, otp: enteredOtp } = req.body
+
+    if (!email || !enteredOtp) {
+        res.status(400).json({ message: "email and otp both required" })
+        return;
+    }
+
+    const otpkey = `otp:${email}`
+
+    const storedotp = await redisClient.get(otpkey)
+
+    if (!storedotp || storedotp !== enteredOtp) {
+        res.status(400).json({ message: "Invalid or expired otp" })
+        return;
+    }
+
+await redisClient.del(otpkey)
+let user = await User.findOne({email})
+
+if(!user){
+    const name = email.slice(0,8);
+    user = await User.create({name,email});
+}
+const token =generateToken(user);
+res.json({
+    message:"user verified",
+    user,
+    token
+});
+
+}) 
+
