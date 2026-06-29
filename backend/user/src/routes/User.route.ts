6d86b1c3b7c049baaa5c446fc7f657afc456300f
Lekha <lekhadalvi08@gmail.com>
2026-06-29 16:07:24 +0530

    Change user retrieval routes from POST to GET for improved RESTful API design

--- backend/user/src/routes/User.route.ts
+++ backend/user/src/routes/User.route.ts
@@ -7,8 +7,8 @@ const Userrouter = express.Router()
 Userrouter.post("/login",LoginController)
 Userrouter.post("/verify",VerifyController)
 Userrouter.get("/me",isAuth,myProfile)
-Userrouter.post("/users/all",isAuth,getAllUsers)
-Userrouter.post("/user/:id",getAUser)
+Userrouter.get("/users/all",isAuth,getAllUsers)
+Userrouter.get("/user/:id",getAUser)
 Userrouter.get("/updatename",isAuth,updateName)
 
 export default Userrouter
