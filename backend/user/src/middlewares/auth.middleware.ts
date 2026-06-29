4027a53996ae3511ae70a0388fc6fccfa2aaa1e8
Lekha <lekhadalvi08@gmail.com>
2026-06-29 14:16:10 +0530

    Add authentication middleware and update user routes to include verification

---
+++ backend/user/src/middlewares/auth.middleware.ts
@@ -0,0 +1,32 @@
+import type { Request } from "express";
+import { TryCatch } from "../components/reuse_code.js";
+import type { IUser } from "../models/User.models.js";
+import jwt, { type JwtPayload } from "jsonwebtoken"
+
+export interface AuthenticatedRequest extends Request{
+    user?: IUser | null;
+}
+
+export const isAuth = TryCatch(async(req: AuthenticatedRequest, res, next)=>{
+
+    const authHeader = req.headers.authorization
+    if (!authHeader ||!authHeader.startsWith("Bearer ")){
+        res.status(400).json({message:"please login "})
+        return; 
+    }
+
+    const token = authHeader.split(" ")[1]
+    if (!token) {
+        res.status(401).json({message:"invalid token"})
+        return;
+    }
+
+    const decodevalue = jwt.verify(token ,process.env.JWT_SECRET as string) as JwtPayload
+    if(!decodevalue || !decodevalue.user){
+        res.status(401).json({message:"invalid token"})
+        return;
+    }
+
+    req.user = decodevalue.user
+    next();
+})
--- backend/user/src/routes/User.route.ts
+++ backend/user/src/routes/User.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { LoginController } from '../controllers/User.controller.js';
+import { LoginController, VerifyController } from '../controllers/User.controller.js';
 
 const Userrouter = express.Router()
 
 Userrouter.post("/login",LoginController)
+Userrouter.post("/verify",VerifyController)
 
 export default Userrouter
